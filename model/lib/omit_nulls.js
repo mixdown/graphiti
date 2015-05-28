09c25c3be8ae435f740fc7637c1eab9af2116a69
Michael Angstadt <mangstadt@cbancnetwork.com>
2015-05-28 11:28:49 -0500

    v1.2.4, need to omit nulls within arrays too

--- model/lib/omit_nulls.js
+++ model/lib/omit_nulls.js
@@ -1,7 +1,12 @@
 var _ = require('lodash');
 
-module.exports = function(obj) {
-  return _.omit(obj || {}, function(value) {
+var omit_nulls = module.exports = function (obj) {
+  return _.omit(obj || {}, function (value) {
+
+    if (value instanceof Array) {
+      value = omit_nulls(value);
+    }
+
     return value === null;
   });
 };
