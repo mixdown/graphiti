406e034d79e308cd608befda0e4e7d910bbe493b
Michael Angstadt <mangstadt@cbancnetwork.com>
2015-05-28 11:53:35 -0500

    v1.2.5, else wrapper for non-recursive return

--- model/lib/omit_nulls.js
+++ model/lib/omit_nulls.js
@@ -2,11 +2,12 @@ var _ = require('lodash');
 
 var omit_nulls = module.exports = function (obj) {
   return _.omit(obj || {}, function (value) {
-
+    //if this is an array - dig deeper
     if (value instanceof Array) {
       value = omit_nulls(value);
+    } else {
+      return value === null;
     }
 
-    return value === null;
   });
 };
